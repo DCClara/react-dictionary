9bfe11988db6ef054b2d8c03bc35110a0debbd38
DCClara <141446800+DCClara@users.noreply.github.com>
2023-12-13 18:56:31 -0500

    created loading component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="forest" />
         </main>
         <footer className="App-footer">Coded by Danielle Casa</footer>
       </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,35 +3,51 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
-    // documentation: https://www.shecodes.io/learn/apis/dictionary
-    //let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=book&key=d494296b1e5a34o4t344f5460f276a60`;
-
+  function search() {
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  return (
-    <div className="Dictionary">
-      <form className="DictionaryForm" onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
 
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form className="DictionaryForm" onSubmit={handleSubmit}>
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <div className="hint">
+            suggested words: grounding, above, grateful, stone, thirst
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading..";
+  }
 }
