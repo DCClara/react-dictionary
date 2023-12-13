086199cd5eb6852438c8f4b43e4fd4a0678f80e6
DCClara <141446800+DCClara@users.noreply.github.com>
2023-12-13 17:25:33 -0500

    rendered the word phonetic

--- src/Results.js
+++ src/Results.js
@@ -1,16 +1,27 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
-  
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          if (phonetic.audio) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          } else {
+            return <></>;
+          }
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
-                <Meaning meaning={meaning} />
+              <Meaning meaning={meaning} />
             </div>
           );
         })}
