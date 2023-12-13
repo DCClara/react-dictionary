dbe1197f4e318d3f533259fe3e29b3b7b1124bae
DCClara <141446800+DCClara@users.noreply.github.com>
2023-12-13 13:29:07 -0500

    added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword} definition`);
+
+    // documentation: https://www.shecodes.io/learn/apis/dictionary
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=book&key=d494296b1e5a34o4t344f5460f276a60`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
