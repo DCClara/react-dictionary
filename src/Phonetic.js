8746d61d36349964fdedf508120edd2bff353138
DCClara <141446800+DCClara@users.noreply.github.com>
2023-12-13 23:30:12 -0500

    added audio controls

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,12 +4,12 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-    <span className="text">
-      {props.phonetic.text}
-      </span>
+      <audio controls src={props.phonetic.audio}>
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          Listen
+        </a>
+      </audio>
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
